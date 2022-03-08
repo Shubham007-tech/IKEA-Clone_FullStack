3d6ef6086f3fd8e9d62e057c1f6a35b2a9fc7d80
Shubham007-tech <91582453+Shubham007-tech@users.noreply.github.com>
2022-03-08 13:06:13 +0530

    deployed

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { store } from "./Redux/store"
+import { Provider as ReduxProvider } from "react-redux"
+import { BrowserRouter as ReactRouter } from "react-router-dom"
+import { CookiesProvider } from "react-cookie"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CookiesProvider>
+      <ReduxProvider store={store}>
+        <ReactRouter>
+          <App />
+        </ReactRouter>
+      </ReduxProvider>
+    </CookiesProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
